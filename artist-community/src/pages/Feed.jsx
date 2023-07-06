a46cbe949ee12f3c07a4843c773b3ff7314dbd9d
Yoonjit <jisu9083@naver.com>
2023-07-06 18:56:22 +0900

    풋터 정말 간단하게 만들기

--- artist-community/src/pages/Feed.jsx
+++ artist-community/src/pages/Feed.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
-const Feed = () => {
+import App from "../App";
+import Header from "./Header";
+import Navigator from "./Navigator";
+import Footer from "./Footer";
+
+function Feed() {
   return (
     <>
-      <p>hello world</p>
+      {/* <Header></Header> */}
+      {/* <Navigator></Navigator> */}
+      <p>Hello world</p>
+      <Footer></Footer>
     </>
   );
-};
+}
 
 export default Feed;
---
+++ artist-community/src/pages/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Row, Col } from "reactstrap";
+
+import styles from "../style/Footer.css";
+
+const Footer = () => {
+  return (
+    <div id="main-footer">
+      <p>
+        Terms of use &nbsp;&nbsp;|&nbsp;&nbsp; Service operation policy
+        &nbsp;&nbsp;|&nbsp;&nbsp; Premium service terms of use
+        &nbsp;&nbsp;|&nbsp;&nbsp; Privacy policy &nbsp;&nbsp;|&nbsp;&nbsp;
+        Cookie policy &nbsp;&nbsp;|&nbsp;&nbsp; Cookie Settings
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
