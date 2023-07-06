e6741530e3c09270c9e1a9e6eaa64859084df282
Yoonjit <jisu9083@naver.com>
2023-07-06 21:40:44 +0900

    div 박스, 슬라이드 화살표 추가

--- artist-community/src/pages/Body.jsx
+++ artist-community/src/pages/Body.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 import styles from "../style/Body.css";
 import image from "../assets/feed-back.svg";
+import left from "../assets/left-arrow.png";
+import right from "../assets/right-arrow.png";
 
 const Body = () => {
   return (
+    // 이미지 넣기
     // <>
     //   <div>
     //     <img src={image} />
@@ -14,13 +17,25 @@ const Body = () => {
     <div
       style={{
         backgroundImage: `url(${image})`,
-        // backgroundSize: "100%",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
         width: "100vw",
         height: "260px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
-    ></div>
+    >
+      <div>
+        <img style={{ marginRight: "30px" }} src={left} />
+      </div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div>
+        <img style={{ marginLeft: "30px" }} src={right} />
+      </div>
+    </div>
   );
 };
 
