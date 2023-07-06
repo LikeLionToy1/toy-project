b7f6fd88108558cef749f1a171e36f8a79ad5aee
Yoonjit <jisu9083@naver.com>
2023-07-06 21:22:11 +0900

    배경 이미지 오류 해결

--- artist-community/src/pages/Body.jsx
+++ artist-community/src/pages/Body.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { Row, Col } from "reactstrap";
 
 import styles from "../style/Body.css";
 import image from "../assets/feed-back.svg";
 
 const Body = () => {
   return (
-    <>
-      <div>
-        <img src={image} />
-      </div>
-    </>
+    // <>
+    //   <div>
+    //     <img src={image} />
+    //   </div>
+    // </>
+
+    <div
+      style={{
+        backgroundImage: `url(${image})`,
+        // backgroundSize: "100%",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        width: "100vw",
+        height: "260px",
+      }}
+    ></div>
   );
 };
 
