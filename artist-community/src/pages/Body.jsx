cf18e16be0685e392ff4b4c566e6e63649967ba7
Yoonjit <jisu9083@naver.com>
2023-07-07 01:15:44 +0900

    피드 윗부분 아티스트가 전하는 말 퍼블리싱만

--- artist-community/src/pages/Body.jsx
+++ artist-community/src/pages/Body.jsx
@@ -4,6 +4,8 @@ import styles from "../style/Body.css";
 import image from "../assets/feed-back.svg";
 import left from "../assets/left-arrow.png";
 import right from "../assets/right-arrow.png";
+import check from "../assets/check.png";
+import artist from "../assets/artist_profile.png";
 
 const Body = () => {
   return (
@@ -29,9 +31,40 @@ const Body = () => {
       <div>
         <img style={{ marginRight: "30px" }} src={left} />
       </div>
-      <div className="box"></div>
-      <div className="box"></div>
-      <div className="box"></div>
+
+      <div className="box">
+        <div className="profile">
+          <img style={{ width: "43px", height: "43px" }} src={artist} />
+        </div>
+        <h3>MinHyeok</h3>
+        <img
+          style={{ width: "15px", height: "15px", marginLeft: "10px" }}
+          src={check}
+        />
+      </div>
+
+      <div className="box">
+        <div className="profile">
+          <img style={{ width: "43px", height: "43px" }} src={artist} />
+        </div>
+        <h3>MinHyeok</h3>
+        <img
+          style={{ width: "15px", height: "15px", marginLeft: "10px" }}
+          src={check}
+        />
+      </div>
+
+      <div className="box">
+        <div className="profile">
+          <img style={{ width: "43px", height: "43px" }} src={artist} />
+        </div>
+        <h3>MinHyeok</h3>
+        <img
+          style={{ width: "15px", height: "15px", marginLeft: "10px" }}
+          src={check}
+        />
+      </div>
+
       <div>
         <img style={{ marginLeft: "30px" }} src={right} />
       </div>
